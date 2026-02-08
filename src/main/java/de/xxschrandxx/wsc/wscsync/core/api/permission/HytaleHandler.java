ccf2174497aeff2b99b032b6bcb036c5251ef5cc
xXSchrandXx <marvinschrand@yahoo.de>
2026-02-08 20:58:47 +0100

    Add full hytale permission support

--- src/main/java/de/xxschrandxx/wsc/wscsync/core/api/permission/HytaleHandler.java
+++ src/main/java/de/xxschrandxx/wsc/wscsync/core/api/permission/HytaleHandler.java
@@ -1,10 +1,16 @@
 package de.xxschrandxx.wsc.wscsync.core.api.permission;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.UUID;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 import com.hypixel.hytale.server.core.permissions.PermissionsModule;
+import com.hypixel.hytale.server.core.permissions.provider.HytalePermissionsProvider;
 
 import de.xxschrandxx.wsc.wscbridge.core.api.command.ISender;
 import de.xxschrandxx.wsc.wscsync.core.api.ISyncCoreAPI;
@@ -18,7 +24,32 @@ public class HytaleHandler implements IPermissionHandler {
     }
 
     public ArrayList<String> groupList() {
-        throw new UnsupportedOperationException("Cannot list groups.");
+        ArrayList<String> groups = new ArrayList<String>(HytalePermissionsProvider.DEFAULT_GROUP_LIST);
+        try {
+            FileReader fileReader = new FileReader(HytalePermissionsProvider.PERMISSIONS_FILE_PATH);
+            JsonReader jsonReader = new JsonReader(fileReader);
+            try {
+                JsonObject root = JsonParser.parseReader(jsonReader).getAsJsonObject();
+                if (root.has("groups")) {
+                    JsonObject jsonGroups = root.getAsJsonObject("groups");
+                    groups = new ArrayList<String>(jsonGroups.keySet());
+                }
+                jsonReader.close();
+            } catch (Throwable throwable) {
+                try {
+                    jsonReader.close();
+                } catch (Throwable throwable1) {
+                    throwable.addSuppressed(throwable1);
+                } 
+                this.api.log("Could not load groups: " + throwable.toString());
+            }
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            this.api.log("Permissions file not found: " + HytalePermissionsProvider.PERMISSIONS_FILE_PATH);
+        } catch (Throwable throwable) {
+            this.api.log("Error reading permissions file: " + throwable.toString());
+        }
+        return groups;
     }
 
     public ArrayList<String> getUsersGroups(ISender<?> sender) {
